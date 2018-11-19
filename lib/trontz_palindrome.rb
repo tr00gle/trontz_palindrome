a7ed9c90dfe265c38cd6dd89955cf4b7ff8a4194
trontz <rtrontz@gmail.com>
2018-11-18 18:42:24 -0800

    initialize repository

---
+++ lib/trontz_palindrome.rb
@@ -0,0 +1,6 @@
+require "trontz_palindrome/version"
+
+module TrontzPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/trontz_palindrome/version.rb
@@ -0,0 +1,3 @@
+module TrontzPalindrome
+  VERSION = "0.1.0"
+end
